f0d17359e03d9f11d1440339a3317b78c4ea2c38
SehwarMaryam <maryam.dev93@gmail.com>
2025-02-15 18:56:05 +0500

    Refactor star pattern generation to create a right-aligned triangle

--- practice.cpp
+++ practice.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main(){
-    for(int i=5; i>=1; i--){
-        for(int j=i; j>0; j--){
-            cout<<"* ";
-        }
+    int n=5;
+    for(int i=1; i<=n; i++){
+    for(int k=n-i; k>0; k--){
+        cout<<" ";
+    }
+    for(int j=1; j<=i; j++){
+    cout<<"* ";
+ } 
+    
         cout<<endl;
     }
-
  return 0;   
 }
