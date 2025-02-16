e23a057ced219c32c5402166670584ca16ee0d6e
SehwarMaryam <maryam.dev93@gmail.com>
2025-02-16 15:53:14 +0500

    Add struct for Person and implement input/output for person details

--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,31 @@
 #include<iostream>
 using namespace std;
+ 
+struct Person{
+    char name[100];
+    int age;
+    double salary;
+};
 
 int main(){
-    int n=5;
-    for(int i=1; i<=n; i++){
-    for(int k=n-i; k>0; k--){
-        cout<<" ";
-    }
-    for(int j=1; j<=i; j++){
-    cout<<"* ";
- } 
-    
-        cout<<endl;
-    }
+    Person p1;
+    cout<<"Enter persons details"<<endl;
+
+    cout<<"Perons Name:"<<endl;
+    cin.getline(p1.name, 100);
+
+    cout<<"Person Age:"<<endl;
+    cin>>p1.age;
+
+    cout<<"Person Salary:"<<endl;
+    cin>>p1.salary;
+
+    cout<<"Person Details are:"<<endl;
+
+    cout<<"Name:"<<p1.name<<endl;
+    cout<<"Age:"<<p1.age<<endl;
+    cout<<"Salary:"<<p1.salary<<endl;
+
  return 0;   
 }
+
