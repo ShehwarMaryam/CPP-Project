d0c0f75badf7cbf3e78de73bbb163dd967affbb5
SehwarMaryam <maryam.dev93@gmail.com>
2025-02-16 16:52:16 +0500

    Add Car and Engine structs with function to print car details

--- practice.cpp
+++ practice.cpp
@@ -1,29 +1,36 @@
 #include<iostream>
 using namespace std;
- 
-struct Smartphone{
-    string name;
-    int storage;
-    string color;
-    float price;
 
+struct Engine {
+    string engineType;
+    int horsepower;
 };
 
-void printSmartphoneInfo(Smartphone smartphone1){
-    cout<<"The name of the smartphone is "<<smartphone1.name<<endl;
-    cout<<"The storage of the smartphone is "<<smartphone1.storage<<endl;
-    cout<<"The color of the smartphone is "<<smartphone1.color<<endl;
-    cout<<"The price of the smartphone is "<<smartphone1.price<<endl;
-}
-int main(){
-    Smartphone smartphone1;
-    smartphone1.name = "Samsung Galaxy S21";
-    smartphone1.storage = 128;
-    smartphone1.color = "Phantom Gray";
-    smartphone1.price = 799.99;
-    printSmartphoneInfo(smartphone1);
-    
-   
- return 0;   
+struct Car {
+    string brand;
+    string model;
+    int year;
+    Engine engine;  // struct inside another struct
+};
+
+void printCarInfo(Car car) {
+    cout << "Car Brand: " << car.brand << endl;
+    cout << "Car Model: " << car.model << endl;
+    cout << "Car Year: " << car.year << endl;
+    cout << "Engine Type: " << car.engine.engineType << endl;
+    cout << "Engine Horsepower: " << car.engine.horsepower << endl;
 }
 
+int main() {
+    Car myCar;  // Declare the car object
+
+    myCar.brand = "Ford";
+    myCar.model = "Mustang";
+    myCar.year = 2023;
+    myCar.engine.engineType = "V8";  // Accessing the inner struct's members
+    myCar.engine.horsepower = 450;
+
+    printCarInfo(myCar);  // Pass the object to the function
+
+    return 0;
+}
